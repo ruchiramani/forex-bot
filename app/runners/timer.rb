adb72f3b9750aac70492b79be1890d2042592d07
TimoSci <me@timosci.net>
2015-12-14 18:33:36 -0500

    created timer

---
+++ app/runners/timer.rb
@@ -0,0 +1,41 @@
+require 'pry'
+
+class Timer
+
+ @@sleep = 5
+
+ attr_reader :trading_time
+
+ def initialize(trading_time,bot)
+   t = Time.now
+   t = t - t.sec
+   t = t - (t.min*60)
+   t = t - (t.hour*3600)
+   t = t + (trading_time*3600)
+   @trading_time = t
+   add_day
+ end
+
+ def start
+   loop do
+     sleep @@sleep
+     now = Time.now
+     if now > trading_time
+       act
+       add_day
+     end
+   end
+ end
+
+ def add_day
+   @trading_time += (3600*24)
+ end
+
+ def act
+   puts "waking up the bot..."
+   bot.decide
+ end
+
+end
+
+binding.pry
---
+++ config/environment.rb
@@ -0,0 +1,12 @@
+# load our external dependencies
+
+#require 'json'
+
+require 'pry'
+require 'nokogiri'
+require 'rest-client'
+require 'bundler/setup'
+Bundler.require
+
+#require_relative '../lib/'
+#binding.pry
