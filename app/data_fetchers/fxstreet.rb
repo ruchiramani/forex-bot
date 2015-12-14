d9364f8581925a5154da0b4018e2cde34fb892dc
TimoSci <me@timosci.net>
2015-12-14 15:53:21 -0500

    created scraper for fxstreet.com

---
+++ app/data_fetchers/fxstreet.rb
@@ -0,0 +1,43 @@
+require 'pry'
+
+require 'nokogiri'
+require 'rest-client'
+
+
+
+
+
+class FXStreetScraper
+
+  attr_reader :rates, :date
+
+  def initialize
+    @date = Time.now
+    @rates = self.rates_now
+    # @rates
+  end
+
+  def rates_now(url = "http://www.fxstreet.com/rates-charts/forex-rates/")
+
+    html = RestClient.get(url)
+    doc = Nokogiri::HTML(html)
+
+    # prices = doc.css(".table-ttpushrates.table-rates").css(".ttratescaglink").css("td")[3].text
+    prices = doc.css(".table-ttpushrates.table-rates").css(".ttratescaglink").css("td")
+
+    prices_hash = {}
+    prices[1..4].each do |price|
+      #  binding.pry
+      id = price.attribute("id").value
+      type = id.scan(/^(\w+)_/).flatten.first
+      value = price.text.to_f
+      prices_hash[type] = value
+    end
+    prices_hash
+
+  end
+
+end
+
+
+binding.pry
